a7af413b338ab9121f1e5cea671e272bb2c5dbfd
Ilya Yarosh <ilyayarosh@ilyas-macbook-pro.home>
2023-02-22 23:10:13 +0100

    The second task 'Number from Range' is done

--- Lesson_1/HelloName_Lesson/HelloName_Lesson/Program.cs
+++ Lesson_1/HelloName_Lesson/HelloName_Lesson/Program.cs
@@ -3,7 +3,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Task1();
+        Task2();
     }
     public static void Lesson1()
     {
@@ -74,6 +74,40 @@ internal class Program
                 Console.WriteLine("Wrong arithmetic sign, please choose from: '+' '-' '/' and '*'");
                 break;
         }
+    }
+    public static void Task2()
+    {
+        Console.WriteLine("Enter the number from 0 to 100: ");
+        byte number = byte.Parse(Console.ReadLine());
+
+
+        if ((number >= 0) && (number <= 14))
+        {
+            Console.WriteLine("The number is in the Range [0-14]");
+        }
+        else if ((number >= 15) && (number <= 35))
+
+        {
+            Console.WriteLine("The number is in the Range [15-35]");
+        }
+        else if ((number >= 36) && (number <= 49))
+
+        {
+            Console.WriteLine("The number is in the Range [36-49]");
+        }
+        else if ((number >= 50) && (number <= 100))
+
+        {
+            Console.WriteLine("The number is in the Range [50-100]");
+        }
+        else
+        {
+            Console.WriteLine("The number is not in the any Range");
+        }
+
+
+
+
     }
 
 
