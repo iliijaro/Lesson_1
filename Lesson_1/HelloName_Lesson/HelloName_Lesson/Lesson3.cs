9ed5932a576624803dd6a8161fe668ccde48d825
Ilya Yarosh <ilyayarosh@ilyas-macbook-pro.home>
2023-02-25 13:59:24 +0100

    Added 'test' console writeline for checking the performance of new Class

--- Lesson_1/HelloName_Lesson/HelloName_Lesson/Lesson3.cs
+++ Lesson_1/HelloName_Lesson/HelloName_Lesson/Lesson3.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 namespace HelloName_Lesson
 {
-	public class Lesson3
+    /// <summary>
+    /// Создайте массив целых чисел. Напишете программу,
+	/// которая выводит сообщение о том, входит ли заданное число в массив или нет.Пусть число для поиска задается с консоли.
+    /// </summary>
+    internal class Lesson3
 	{
-		public Lesson3()
-		{
-		}
-	}
+        public static void HomeTask3()
+        {
+            Console.WriteLine("I am working");
+        }
+    }
 }
 
--- Lesson_1/HelloName_Lesson/HelloName_Lesson/Program.cs
+++ Lesson_1/HelloName_Lesson/HelloName_Lesson/Program.cs
@@ -1,11 +1,11 @@
 ﻿
 internal class Program
 {
-    private static void Main(string[] args)
+    static void Main(string[] args)
     {
-        Task1();
+        HelloName_Lesson.Lesson3.HomeTask3(); 
     }
-    public static void Lesson1()
+    static void Lesson1()
     {
         string name = Console.ReadLine();
         Console.WriteLine("Hello " + name);
