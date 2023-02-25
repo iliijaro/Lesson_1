8baf5adf34aebafac277ffab4fce878eee6d5923
Ilya Yarosh <ilyayarosh@ilyas-macbook-pro.home>
2023-02-25 15:35:30 +0100

    The first HomeTask about the number existing in the array is done

--- Lesson_1/HelloName_Lesson/HelloName_Lesson/Lesson3.cs
+++ Lesson_1/HelloName_Lesson/HelloName_Lesson/Lesson3.cs
@@ -9,7 +9,28 @@ namespace HelloName_Lesson
 	{
         public static void HomeTask3()
         {
-            Console.WriteLine("I am working");
+            int[] Array = {7,2,3,4,7,1,4,7};
+
+            Console.WriteLine("Enter the number to check its existence in the Array:" + "\n");
+            float NumberFromConsole = float.Parse(Console.ReadLine());
+
+            int count = 0;
+
+            for (int i = 0; i < Array.Length; i++)
+            {
+                if (NumberFromConsole == Array[i])
+                {
+                    count++;
+                }   
+            }
+            if (count > 0)
+            {
+                Console.WriteLine("\n" + "Current number exists in the Array " + count + " time(s)");
+            }
+            else
+            {
+                Console.WriteLine("\n" + "Current number does not exist in the Array");
+            }
         }
     }
 }
