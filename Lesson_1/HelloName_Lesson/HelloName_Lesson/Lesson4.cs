16a00e3f84de2ba5570ef9a6420330e1bf2de4ae
Ilya Yarosh <ilyayarosh@ilyas-macbook-pro.home>
2023-03-03 10:05:21 +0100

    The hometask 'Replacing test to testing' is done

--- Lesson_1/HelloName_Lesson/HelloName_Lesson/Lesson4.cs
+++ Lesson_1/HelloName_Lesson/HelloName_Lesson/Lesson4.cs
@@ -1,11 +1,24 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+
 namespace HelloName_Lesson
 {
-	public class Lesson4
+	internal class Lesson4
 	{
-		public Lesson4()
+        /// <summary>
+        /// Заменить в строке все вхождения 'test' на 'testing'. Удалить из текста все символы, являющиеся цифрами.
+        /// </summary>
+        
+        public static void HomeTask4()
 		{
-		}
-	}
+            string NewString = "test and 1test 231231 best test";
+
+            NewString = NewString.Replace("test", "testing");
+
+            NewString = Regex.Replace(NewString, "[0-9]", "", RegexOptions.IgnoreCase);
+
+            Console.WriteLine(NewString);
+        }
+    }
 }
 
--- Lesson_1/HelloName_Lesson/HelloName_Lesson/Program.cs
+++ Lesson_1/HelloName_Lesson/HelloName_Lesson/Program.cs
@@ -3,7 +3,7 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        HelloName_Lesson.Lesson3.HomeTask3_4(); 
+        HelloName_Lesson.Lesson4.HomeTask4(); 
     }
     static void Lesson1()
     {
