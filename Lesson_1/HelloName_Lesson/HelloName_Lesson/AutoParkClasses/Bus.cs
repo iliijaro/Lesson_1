4799bdc768bc3207c11511389d51d4b3759ee5f8
Ilya Yarosh <ilyayarosh@ilyas-macbook-pro.home>
2023-03-17 00:46:46 +0100

    The task 'Public transport' is done + cosmetic fixes

---
+++ Lesson_1/HelloName_Lesson/HelloName_Lesson/AutoParkClasses/Bus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace HelloName_Lesson.AutoParkClasses
+{
+    public class Bus : Transport
+    {
+        public Bus(string destination, int transportNumber, string departureTime, int amountOfSeats)
+        {
+            this.destination = destination;
+            this.transportNumber = transportNumber;
+            this.departureTime = departureTime;
+            this.amountOfSeats = amountOfSeats;
+        }
+
+        public override void GetInfo()
+        {
+            Console.WriteLine($"The bus Number {transportNumber} arrives at {departureTime} is going to {destination} and it has only {amountOfSeats} seats ");
+        }
+
+        public override string GetTheTransportType()
+        {
+            return "This is a diesel type";
+        }
+    }
+}
---
+++ Lesson_1/HelloName_Lesson/HelloName_Lesson/AutoParkClasses/Transport.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace HelloName_Lesson.AutoParkClasses
+{
+    abstract public class Transport : IComparable
+    {
+        public string destination { get; set; }
+        public int transportNumber { get; set; }
+        public string departureTime { get; set; }
+        public int amountOfSeats { get; set; }
+
+        public Transport() { }
+
+        public virtual void GetInfo() { }
+
+        abstract public string GetTheTransportType();
+
+        public virtual int CompareTo(object? obj)
+        {
+            if (obj is Transport transport)
+            {
+                return amountOfSeats.CompareTo(transport.amountOfSeats);
+            }
+            return 0;
+        }
+    }
+}
