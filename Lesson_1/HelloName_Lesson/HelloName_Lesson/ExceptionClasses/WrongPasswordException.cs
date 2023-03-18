26b73e29451143e6d9e1c3230b58ccc95593e553
Ilya Yarosh <ilyayarosh@ilyas-macbook-pro.home>
2023-03-18 16:02:26 +0100

    The Task with 'exceptions' for Login Credits is done

---
+++ Lesson_1/HelloName_Lesson/HelloName_Lesson/ExceptionClasses/WrongPasswordException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace HelloName_Lesson.ExceptionClasses
+{
+    public class WrongPasswordException : Exception
+    {
+        public WrongPasswordException(string message) : base(message) { }
+
+        public WrongPasswordException() { }
+    }
+}
+
---
+++ Lesson_1/HelloName_Lesson/HelloName_Lesson/Lesson8.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using HelloName_Lesson.ExceptionClasses;
+namespace HelloName_Lesson
+{
+    public class Lesson8
+    {
+        public static void Task()
+        {
+            NewCredits correctCredits = new("login123", "12345", "12345");
+            var form1 = NewCredits.CheckCredits(correctCredits);
+            Console.WriteLine(form1 + "\n");
+
+            NewCredits incorrectLogin = new("L ogi n", "12345", "12345");
+            var form2 = NewCredits.CheckCredits(incorrectLogin);
+            Console.WriteLine(form2 + "\n");
+
+            NewCredits incorrectPassword = new("login123", "12345678911234567891", "12345678911234567891");
+            var form3 = NewCredits.CheckCredits(incorrectPassword);
+            Console.WriteLine(form3 + "\n");
+
+            NewCredits notMatchedPassword = new("login123", "123456789", "12345678911234567891");
+            var form4 = NewCredits.CheckCredits(notMatchedPassword);
+            Console.WriteLine(form4 + "\n");
+        }
+    }
+}
