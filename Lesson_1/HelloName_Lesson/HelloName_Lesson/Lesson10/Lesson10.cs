ebfd703339eccd5ed3a567e8d8da46f3a38003f1
Ilya Yarosh <ilyayarosh@ilyas-macbook-pro.home>
2023-03-30 01:48:43 +0200

    The Generic task is done

---
+++ Lesson_1/HelloName_Lesson/HelloName_Lesson/Lesson10/Lesson10.cs
@@ -0,0 +1,26 @@
+﻿namespace HelloName_Lesson.Lesson10
+{
+    public class Lesson10
+    {
+        public static void Task1()
+        {
+            MyList<string> newArray = new MyList<string>();
+            newArray.Add("Hello");
+            newArray.Add("I");
+            newArray.Add("am");
+            newArray.Add("Array");
+
+            newArray.Delete("I");
+            newArray.Delete("Hello");
+
+            Console.WriteLine(newArray.GetElement(1) + "\n");
+
+            Console.WriteLine(newArray.Count() + "\n");
+
+            for (int i = 0; i < newArray.Count(); i++)
+            {
+                Console.WriteLine(newArray[i]);
+            }
+        }
+    }
+}
